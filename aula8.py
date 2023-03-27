5aa7e64f5bd857e0409ab0f27a308d07b9026c6a
Bruno Klippel <brunoklippelc@gmail.com>
2023-03-27 16:11:00 -0300

    first commit

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = 'Bruno'
+sobrenome = 'Klippel'
+idade = 29
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.90
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
